f180dfe8530f7aa45224bc595075d5e663d27787
ASCIIX <36745408+ASCIIX@users.noreply.github.com>
2021-11-21 15:25:05 +0800

    Update Mess around.js

--- js/Mess around.js
+++ js/Mess around.js
@@ -6,15 +6,14 @@ function GetServerTime()
  	var endDate = new Date("01/01/2022 00:00:00");
 	var end = endDate.getTime( );
 
-    var leftTime = end - now;
+    	var leftTime = end - now;
 
-    var d,h,m,s;
- 	date.setTime(date.getTime()+250);
-
-	d = Math.floor(leftTime/1000/60/60/24);
- 	h = Math.floor(leftTime/1000/60/60%24);
- 	m = Math.floor(leftTime/1000/60%60);
-    s = Math.round(leftTime/1000%60);
+    	var d,h,m,s;
+ 		date.setTime(date.getTime()+250);
+		d = Math.floor(leftTime/1000/60/60/24);
+ 		h = Math.floor(leftTime/1000/60/60%24);
+ 		m = Math.floor(leftTime/1000/60%60);
+    		s = Math.round(leftTime/1000%60);
  	document.getElementById("date").innerHTML = d + " 天 ";
  	document.getElementById("time").innerHTML = h + " 小时 " + m + " 分钟 " + s + " 秒 ";
 }
